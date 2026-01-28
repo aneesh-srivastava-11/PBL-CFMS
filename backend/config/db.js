30d3ad9cc4dd4bf620e459880d404552f130d584
aneesh-srivastava-11 <aneeshplays1175@gmail.com>
2026-01-28 18:44:06 +0530

    v22

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,43 +3,44 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-    ?new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: false,
-    pool: {
-        max: 2,
-        min: 0,
-        acquire: 3000,
-        idle: 0
-    },
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-})
-    : new Sequelize(
-    process.env.DB_NAME || 'course_file_db',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '',
-    {
-        host: process.env.DB_HOST || 'localhost',
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
         logging: false,
-        // Serverless Connection Pooling
         pool: {
-            max: 2,     // Limit max connections per lambda container
+            max: 2,
             min: 0,
             acquire: 3000,
-            idle: 0     // Terminate idle connections immediately to save resources
+            idle: 0
         },
         dialectOptions: {
-            // Keep Keep-Alive on to re-use connections
-            keepAlive: true,
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
-    }
-);
+    })
+    : new Sequelize(
+        process.env.DB_NAME || 'course_file_db',
+        process.env.DB_USER || 'root',
+        process.env.DB_PASSWORD || '',
+        {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'postgres',
+            logging: false,
+            // Serverless Connection Pooling
+            pool: {
+                max: 2,     // Limit max connections per lambda container
+                min: 0,
+                acquire: 3000,
+                idle: 0     // Terminate idle connections immediately to save resources
+            },
+            dialectOptions: {
+                // Keep Keep-Alive on to re-use connections
+                keepAlive: true,
+            }
+        }
+    );
 
 const connectDB = async () => {
     try {
